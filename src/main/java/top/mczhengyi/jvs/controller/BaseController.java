dc851d9dc931249619d68879d9f9306e6853276c
数字媒体工作坊 <shuzimeitigongzuofang@shuzimeitigongzuofangdeMac-Pro.local>
2020-12-22 21:41:06 +0800

    修改了CORS的逻辑

--- src/main/java/top/mczhengyi/jvs/controller/BaseController.java
+++ src/main/java/top/mczhengyi/jvs/controller/BaseController.java
@@ -3,6 +3,5 @@ package top.mczhengyi.jvs.controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import top.mczhengyi.jvs.utils.ConfigUtils;
 
-@CrossOrigin()
 public class BaseController {
 }
